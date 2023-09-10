54e842d553b9171ca916509f875bd45e4ec6eb61
ArinaSimka <141169470+ArinaSimka@users.noreply.github.com>
2023-09-10 19:04:00 +0300

    С# Домашнее задание 2

---
+++ Z2/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+// что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+// ВАРИАНТ РЕШЕНИЯ 1
+
+// Console.Write("Введите число: ");
+// string num = Console.ReadLine();
+// int n = int.Parse(num);
+// if (n < 100)
+// {     Console.WriteLine("Третьей цифры нет"); }
+// else if (n >= 100 && n <= 999) // если 3-значное
+// {     int rez3 = n % 10;
+//     Console.WriteLine($"Третья цифра числа: {rez3}");}
+// else if (n >= 1000 && n <= 9999) // если 4-значное
+// {    int rez4 = (n % 100) /  10;
+//     Console.WriteLine($"Третья цифра числа: {rez4}");}
+// else if (n >= 10000 && n <= 99999) // если 5-значное
+// {    int rez5 = (n % 1000) /  100;
+//     Console.WriteLine($"Третья цифра числа: {rez5}");}
+// else if (n >= 100000 && n <= 999999) // если 6-значное
+// {    int rez6 = (n % 10000) /  1000;
+//     Console.WriteLine($"Третья цифра числа: {rez6}");}
+// else 
+// {    Console.WriteLine("Превышено кол-во символов");}
+
+// ВАРИАНТ РЕШЕНИЯ 2
+
+Console.Write("Введите число: ");
+string num = Console.ReadLine();
+int n = int.Parse(num);
+char [] array = num.ToCharArray();
+
+if (n > 99)
+ {
+    Console.WriteLine($"Третья цифра: {array[2]}");
+    }
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
---
+++ Z3/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+// и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+// ВАРИАНТ РЕШЕНИЯ 1
+
+// Console.Write("Введите число: ");
+// string strNum = Console.ReadLine();
+// int weekDay = int.Parse(strNum);
+
+// if(weekDay==1) {Console.WriteLine("Понедельник");}
+// else if(weekDay==2) {Console.WriteLine("Вторник");}
+// else if(weekDay==3) {Console.WriteLine("Среда");}
+// else if(weekDay==4) {Console.WriteLine("Четверг");}
+// else if(weekDay==5) {Console.WriteLine("Пятница");}
+// else if(weekDay==6) {Console.WriteLine("Суббота");}
+// else if(weekDay==7) {Console.WriteLine("Воскресенье");}
+// else  {Console.WriteLine("неправильная дата");
+// return;}
+// if(weekDay==6 || weekDay==7) {Console.WriteLine("Выходной");}
+// else {Console.WriteLine("Рабочий день");}
+
+
+// ВАРИАНТ РЕШЕНИЯ 2
+
+Console.Write("Введите число: ");
+string strNum = Console.ReadLine();
+int weekDay = int.Parse(strNum);
+string[] array = {"Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"};
+
+if (weekDay > 7 || weekDay < 1 )
+{
+    Console.WriteLine("неправильная дата");
+}
+else 
+{
+    Console.WriteLine($"День недели: {array[weekDay-1]}");
+    if(weekDay==6 || weekDay==7) 
+    {   
+         Console.WriteLine("Выходной");
+    }
+    else 
+    {
+        Console.WriteLine("Рабочий день");
+    }
+}
